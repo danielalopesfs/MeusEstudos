689d5719f93db16a1e0a55e01737d3cb78373e82
= <=>
2022-06-29 23:16:58 -0300

    Meu primeiro projeto em JavaScript

---
+++ index.js
@@ -0,0 +1,36 @@
+function saudar(nome) {
+    console.log("Olá, " + nome + " tudo bem?")
+}
+
+function nomeCompleto(nome, sobrenome) {
+    console.log(`${nome} ${sobrenome}`)
+}
+
+function enderecoCompleto(rua, numero, bairro, estado, pais) {
+    console.log(rua + ", " + "n." + numero + ", " + bairro + ", " + estado + ", " + pais)
+}
+
+const bancoDeDados = {
+    nome: 'Marcos',
+    sobrenome: 'Moura',
+    idade: 26,
+    profissao: 'dev',
+    namorada: 'besta',
+    endereco: {
+        rua: 'Ubá',
+        numero: 59,
+        bairro: 'São Jose',
+        estado: 'Minas Gerais',
+        pais: 'Brasil'
+    }
+}
+
+enderecoCompleto(
+    bancoDeDados.endereco.rua, 
+    bancoDeDados.endereco.numero, 
+    bancoDeDados.endereco.bairro, 
+    bancoDeDados.endereco.estado, 
+    bancoDeDados.endereco.pais
+)
+
+
